199e1c080d59131b6485d4d6ba75348407fbeb9c
ly <664162337@qq.com>
2020-04-18 17:06:07 +0800

    0 配置 xml 文件，启动一个 springmvc 项目。

---
+++ lesson008-springmvc-0xml/src/main/java/top/ybq87/StarterClass.java
@@ -0,0 +1,31 @@
+package top.ybq87;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import top.ybq87.config.SpringMvcApplicationContext;
+import top.ybq87.config.SpringRootApplicationContext;
+
+/**
+ * @author ly
+ * @blog http://www.ybq87.top
+ * @github https://github.com/Lingouzi
+ * @email 664162337@qq.com
+ * @wechat ly19870316 / 公众号：林子曰
+ * @date 2020/4/18
+ */
+public class StarterClass extends AbstractAnnotationConfigDispatcherServletInitializer {
+    
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringRootApplicationContext.class};
+    }
+    
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringMvcApplicationContext.class};
+    }
+    
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
