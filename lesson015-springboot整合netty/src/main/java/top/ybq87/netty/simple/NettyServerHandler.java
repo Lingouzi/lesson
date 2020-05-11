94889acb8e7d0554d5f59d199fc7d7ba8583257a
ly <664162337@qq.com>
2020-05-11 21:52:06 +0800

    netty 进一步测试

--- lesson015-springboot整合netty/src/main/java/top/ybq87/netty/simple/NettyServerHandler.java
+++ lesson015-springboot整合netty/src/main/java/top/ybq87/netty/simple/NettyServerHandler.java
@@ -6,6 +6,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 import java.nio.charset.Charset;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 自定义 handler，需要继承 netty 规定的某个 handlerAdapter
@@ -26,12 +27,33 @@ public class NettyServerHandler extends ChannelInboundHandlerAdapter {
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("server ctx >> " + ctx);
-        System.out.println("thread name >>" + Thread.currentThread().getName());
-        // 这个 bytebuf 是 netty 提供的，不是 nio 中的 bytebuffer
-        ByteBuf byteBuf = (ByteBuf) msg;
-        System.out.println("msg>>" + byteBuf.toString(CharsetUtil.UTF_8));
-        System.out.println("client address>> " + ctx.channel().remoteAddress());
+        // 假设执行了很长时间的任务，就需要异步执行，
+        ctx.channel().eventLoop().execute(() -> {
+            // 休眠几秒钟
+            try {
+                TimeUnit.SECONDS.sleep(10);
+                ctx.writeAndFlush(Unpooled.copiedBuffer("sleep ....", CharsetUtil.UTF_8));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        // 注意，这里是在同一个线程中的，所以是顺序执行，第二个方法是在 30s 之后打印
+        ctx.channel().eventLoop().execute(() -> {
+            // 休眠几秒钟
+            try {
+                TimeUnit.SECONDS.sleep(20);
+                ctx.writeAndFlush(Unpooled.copiedBuffer("sleep ....", CharsetUtil.UTF_8));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        
+        // System.out.println("server ctx >> " + ctx);
+        // System.out.println("thread name >>" + Thread.currentThread().getName());
+        // // 这个 bytebuf 是 netty 提供的，不是 nio 中的 bytebuffer
+        // ByteBuf byteBuf = (ByteBuf) msg;
+        // System.out.println("msg>>" + byteBuf.toString(CharsetUtil.UTF_8));
+        // System.out.println("client address>> " + ctx.channel().remoteAddress());
         // super.channelRead(ctx, msg);
     }
     
